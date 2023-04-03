b2e76820a748f6c499050a86e3a3ba0821b4a238
fmajd6936 <129783236+fmajd6936@users.noreply.github.com>
2023-04-03 20:33:55 +0300

    Create animal_sounds.py

---
+++ animal_sounds.py
@@ -0,0 +1,11 @@
+animal = input("Enter an animal: ")
+if animal.lower() == "dog":
+    print("Woof!")
+elif animal.lower() == "cat":
+    print("Meow!")
+elif animal.lower() == "cow":
+    print("Moo!")
+elif animal.lower() == "sheep":
+    print("Baa!")
+else:
+    print("Sorry, I don't know that animal's sound.")
